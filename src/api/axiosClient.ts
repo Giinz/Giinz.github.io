c0cbb10797ac45132a22291038f8939ff672e4fd
Nguyen Cong Duc <duc1704@gmail.com>
2024-05-08 17:30:03 +0700

    change deploy config

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'http://www.kemapi.somee.com/api/',
+  baseURL: 'https://www.kemapi.somee.com/api/',
   headers: {
     'Content-Type': 'application/json'
   }
